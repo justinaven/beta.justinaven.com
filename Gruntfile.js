45ba789ed4a3983bab3105c85e70615eea812ef2
justinaven <javen81@gmail.com>
2014-11-25 07:50:21 -0600

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,122 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        autoprefixer: {
+          main: {
+            options: {
+              browsers: ['> 1%', 'last 2 versions', 'ie >= 8'],
+              map: true
+            },
+            src: 'css/*.css'
+          }
+        },
+
+        concat: {
+            dist: {
+                files: {
+                    'js/global.js' : [
+                    'js/scripts/logging.js',
+                    'js/scripts/menu.js'
+                    ]
+                }
+            },
+
+            ie: {
+                files: {
+                    'js/ie.js' : [
+                    'vendor/html5shiv/dist/html5shiv.js',
+                    'vendor/selectivizr/selectivizr.js'
+                    ]
+                }
+            }
+        },
+
+        concurrent: {
+            target: {
+                tasks: ['jekyll:server', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+
+        jekyll: {
+            server: {
+                options: {
+                    serve: true
+                }
+            },
+            dev: {
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    sourcemap: 'true'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'sass',
+                    src: ['*.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: [{
+                expand: true,
+                cwd: 'js',
+                src: ['*.js', '!*.min.js'],
+                dest: 'js',
+                ext: '.min.js'
+                }]
+            }
+        },
+
+        watch: {
+            css: {
+                files: 'sass/**/*.scss',
+                tasks: ['sass', 'autoprefixer'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            },
+
+            js: {
+                files: 'js/**/*.js',
+                tasks: ['concat:dist', 'uglify'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            },
+
+            jekyll: {
+                files: ['**/*', '!node_modules/**', '!_site/**'],
+                tasks: ['jekyll:dev'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'concat', 'uglify', 'concurrent:target']);
+
+    // plugin tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jekyll');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+}
---
+++ js/scripts/menu.js
@@ -0,0 +1,75 @@
+// --------------------------------------------------------
+//  menu open button
+// --------------------------------------------------------
+
+var $menu = $('.menu'),
+    $body = $('body');
+
+// open
+$('<button class="menu__button menu__open-button">Menu</button>').insertBefore($menu).on('click', function() {
+    $body.toggleClass('menu-activated');
+});
+
+// close
+$('<button class="menu__button menu__close-button">&times;</button>').prependTo($menu).on('click', function() {
+    $body.toggleClass('menu-activated');
+});
+
+
+
+
+// --------------------------------------------------------
+//  max width on category menu
+// --------------------------------------------------------
+
+$('.bar-nav__link').on('mouseenter', function() {
+    $('.outer', this).css({ 'max-width': $('.inner', this).outerWidth() }); // set the max-width to the size of the inner span
+}).on('mouseleave', function() {
+    $('.outer', this).css({ 'max-width': '' }); // set the widths back to the default value
+});
+
+
+
+
+// --------------------------------------------------------
+//  category menu positioning
+// --------------------------------------------------------
+
+var $stickyNav = $('.site-nav-bar'),
+    stickyNavHeight;
+
+var getNavDimensions = function(){
+    stickyNavHeight = $stickyNav.height();
+}
+
+var stickyNav = function(){
+    if ($(window).height() >= stickyNavHeight) {
+        $stickyNav.addClass('site-nav-bar--fixed').css({ 'margin-top': stickyNavHeight/-2 });
+    } else {
+        $stickyNav.removeClass('site-nav-bar--fixed').css({ 'margin-top': '' });
+    }
+};
+
+getNavDimensions();
+stickyNav();
+
+$(window).resize(function() {
+    getNavDimensions();
+    stickyNav();
+});
+
+
+
+
+// --------------------------------------------------------
+//  action and info menus to the mobile menu
+// --------------------------------------------------------
+
+var menuHTML = '<nav class="secondary-nav"><ul class="menu__sub-nav">';
+
+$('.action-nav a, .info-nav a').each(function() {
+    menuHTML += '<li class="menu__sub-nav__item"><a class="menu__sub-nav__link" href="' + $(this).attr('href') + '">' + $(this).text() + '</a></li>';
+});
+
+menuHTML += '</ul></nav>';
+$(menuHTML).insertAfter('.site-nav', $menu);
